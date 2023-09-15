09a20e7840f6557937bff8085013413c0fb74f97
kleineLoesungen <se.wi@icloud.com>
2023-09-15 06:14:48 +0200

    env host renamed

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -2,7 +2,7 @@ import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: process.env.POSTGRES_HOSTNAME,
+    host: process.env.POSTGRES_HOST,
     port: 5432,
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
